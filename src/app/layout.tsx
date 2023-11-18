a7e48171413c1905be2cdcf3c6f415412578c290
Han Park <hello@hanpark.im>
2023-11-18 20:32:44 +0900

    feat: add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import './globals.css'
 
 export default function RootLayout({
@@ -7,7 +9,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`} />
+        <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+        `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   )
 }
