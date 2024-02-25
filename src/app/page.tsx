a3a27431fb7d2b0c07f418cf497b0a6217923f2a
Han Park <hello@hanpark.im>
2024-02-25 14:18:17 +0900

    refactor: use components for duplicated styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,66 +8,105 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <main className={`max-w-md mx-auto md:max-w-screen-2xl`}>
+    <main className={`max-h-screen overflow-y-scroll snap-y snap-mandatory`}>
       <h1 className={`${inter.className} fixed top-24 mx-[2rem] text-5xl font-bold z-50 md:top-32 md:mx-[4rem] md:text-6xl`}>HAN PARK</h1>
-      <section className={`h-[150vh] mx-[2rem] mb-8 md:mx-[4rem]`}>
-        <div className="sticky top-48 md:top-60">
-          <h2 className={`${inter.className} text-lg font-medium md:text-2xl`}>AN ENTREPRENEUR</h2>
-          <p className="mt-0.5 font-light text-sm md:mt-2 md:text-base">
-            Co-founder of <a className={`${styles.link}`} href="https://deeplyin.com" target="_blank">Deeply</a>, a sound AI startup
-          </p>
-        </div>
-      </section>
+      
+      <Section>
+        <SectionHeading>AN ENTREPRENEUR</SectionHeading>
+        <SectionHeadingDescription>
+          Co-founder of <a className={`${styles.link}`} href="https://deeplyin.com" target="_blank">Deeply</a>, a sound AI startup
+        </SectionHeadingDescription>
+      </Section>
+      
+      <Section>
+        <SectionHeading>A RESEARCHER</SectionHeading>
+        <SectionContentsContainer>
+          <SectionContents>
+            <SectionContentsHeading>Research Interest</SectionContentsHeading>
+            <ul className={`${styles.researchinterest} flex flex-wrap text-xs text-neutral-800 font-light md:text-base`}>
+              <li>usable security</li>
+              <li>authentication</li>
+            </ul>
+          </SectionContents>
+          <SectionContents>
+            <SectionContentsHeading>Publication</SectionContentsHeading>
+            <ol className={`${styles.paperlist} list-decimal list-outside text-xs text-neutral-800 font-light pl-[0.9375rem] md:text-base md:pl-[1.125rem]`}>
+              <li>
+                Myeonghoon Ryu, Hongseok Oh, Suji Lee, Han Park, <a className={`${styles.link}`} href="https://arxiv.org/abs/2401.06913" target="_blank">&#34;Microphone Conversion: Mitigating Device Variability in Sound Event Classification,&#34;</a> <span className={`${styles.source}`}><abbr title="IEEE International Conference on Acoustics, Speech and Signal Processing">ICASSP</abbr> 2024</span>
+              </li>
+            </ol>
+          </SectionContents>
+        </SectionContentsContainer>
+      </Section>
       
 
-      <section className={`h-[150vh] mx-[2rem] mb-8 md:mx-[4rem]`}>
-        <div className="sticky top-48 md:top-60">
-          <h2 className={`${inter.className} text-lg font-medium md:text-2xl`}>A RESEARCHER</h2>
-          <p className="mt-0.5 font-light text-sm md:mt-2 md:text-base">
-            
-          </p>
-
-          <div className="mt-48 md:mt-64">
-            <div className="mb-4 md:mb-8">
-              <h3 className="text-sm font-medium md:text-xl">Research Interest</h3>
-              <ul className={`${styles.researchinterest} flex flex-wrap text-xs text-neutral-800 font-light md:text-base`}>
-                <li>usable security</li>
-                <li>authentication</li>
-              </ul>
-            </div>
-
-            <div className="mb-4 md:mb-8">
-              <h3 className="text-sm font-medium md:text-xl">Publication</h3>
-              <ol className={`${styles.paperlist} list-decimal list-outside text-xs text-neutral-800 font-light pl-[0.9375rem] md:text-base md:pl-[1.125rem]`}>
-                <li>
-                  Myeonghoon Ryu, Hongseok Oh, Suji Lee, Han Park, <a className={`${styles.link}`} href="https://arxiv.org/abs/2401.06913" target="_blank">&#34;Microphone Conversion: Mitigating Device Variability in Sound Event Classification,&#34;</a> <span className={`${styles.source}`}><abbr title="IEEE International Conference on Acoustics, Speech and Signal Processing">ICASSP</abbr> 2024</span>
-                </li>
-              </ol>
-            </div>
-
-          </div>
-        </div>
-      </section>
-      
-
-      <section className={`h-[150vh] mx-[2rem] mb-8 md:mx-[4rem]`}>
-        <div className="sticky top-48 md:top-60">
-          <h2 className={`${inter.className} text-lg font-medium md:text-2xl`}>A DEVELOPER</h2>
-          <p className="mt-0.5 font-light text-sm md:mt-2 md:text-base">
-          </p>
-        </div>
-      </section>
+      <Section>
+        <SectionHeading>A DEVELOPER</SectionHeading>
+        
+      </Section>
 
 
-      <section className={`h-[150vh] mx-[2rem] mb-8 md:mx-[4rem]`}>
-        <div className="sticky top-48 md:top-60">
-          <h2 className={`${inter.className} text-lg font-medium md:text-2xl`}>A PERSON</h2>
-          <p className="mt-0.5 font-light text-sm md:mt-2 md:text-base">
-            who loves writing, design, basketball, and photo
-          </p>
-        </div>
-      </section>
+      <Section>
+        <SectionHeading>A PERSON</SectionHeading>
+        <SectionHeadingDescription>
+          who loves writing, design, and basketball
+        </SectionHeadingDescription>
+      </Section>
 
     </main>
   )
+}
+
+const Section = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <section className={`w-full h-screen snap-start px-[2rem] md:px-[4rem]`}>
+      <div className="pt-48 md:pt-64">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+const SectionHeading = ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => {
+  return <h2 className={`${inter.className} text-lg font-medium md:text-2xl`}>{children}</h2>
+}
+
+const SectionHeadingDescription = ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => {
+  return <p className="mt-0.5 font-light text-sm md:mt-2 md:text-base">{children}</p>
+}
+
+const SectionContentsContainer = ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => {
+  return <div className="mt-48 md:mt-64">{children}</div>
+}
+
+const SectionContents = ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => {
+  return <div className="mb-4 md:mb-8">{children}</div>
+}
+
+const SectionContentsHeading = ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => {
+  return <p className="text-sm font-medium md:text-xl">{children}</p>
 }
